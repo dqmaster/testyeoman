414bbaa48cbabeea7dbf6ac69eeedb713a9dc6e1
unknown <ma@AAL-MA.Admiral.local>
2015-05-15 09:14:15 -0400

    start project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularyeoman2App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularyeoman2App
+ */
+angular.module('angularyeoman2App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularyeoman2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularyeoman2App
+ */
+angular.module('angularyeoman2App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
